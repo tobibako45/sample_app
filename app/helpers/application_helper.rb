1b868893b77e31e4b300f9511381382dd8b17547
tobibako45 <tobibako45@gmail.com>
2018-12-19 01:22:41 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返します
+  def full_title(page_title = '')             # メソッドの定義とオプション値
+    base_title = "Ruby on Rails Tutorial Sample App" # 変数への代入
+
+    if page_title.empty? # 理論値テスト
+      base_title # 暗黙の戻り値
+    else
+      page_title + " | " + base_title # 文字列の結合
+    end
+
+  end
+
 end
