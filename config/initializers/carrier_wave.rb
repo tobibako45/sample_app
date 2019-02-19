ee3c7ff23d5592b60a1d7fb23a3fde309065aedb
tobibako45 <tobibako45@gmail.com>
2019-02-19 22:39:45 +0900

    Change uploader

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws' #←ここを追記
     config.fog_credentials = {
+        # Amazon S3用の設定
         :provider              => 'AWS',
         :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
@@ -10,4 +10,3 @@ if Rails.env.production?
     config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
-
